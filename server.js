c607af785728c44cbdd8ebc598b3d7342d15052e
murad4917 <69457223+murad4917@users.noreply.github.com>
2026-06-16 08:39:07 -0700

    Updated My contact
    Contact Information

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.get('/about', (req, res) => {
     res.send({ ' message': 'I am a student of Evangdi Tec . I am learning web development and I am very excited to learn more about it.' });
 });
 
+app.get('/contact', (req, res) => {
+    res.send({ ' message': 'You can contact me at my email address: murad4917@gmail.com Phone:+251928334917' });
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 }); 
